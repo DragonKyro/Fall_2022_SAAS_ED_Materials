a3f8839d3aca218ddcfecb0701151310b458a421
Ashwin <itisashwin@gmail.com>
2022-11-08 16:53:11 -0800

    final changes to lecture and plotting

--- Fall_2022_Lectures/Lecture_8/plotting.py
+++ Fall_2022_Lectures/Lecture_8/plotting.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from mpl_toolkits.mplot3d import Axes3D
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
 np.random.seed(5)
 trainDataRange = np.array([-3,6])
@@ -81,6 +81,46 @@ def validate(model,trainX,noisyY,numSamples=500,dataRange=[-4,7],showPlot=True,c
         plt.show()
     return error(trueY,predY)
 
+def validateRegularization(model,predictFn,trainX,noisyY,l,numSamples=500,dataRange=[-4,7],showPlot=True,coeffs={},fig_size=(7,5)):
+    dataRange=np.arange(dataRange[0],dataRange[1],0.1)
+    transformed_dataRange = transform_x(dataRange)
+    #Generate x values from the data range
+    inputX = np.random.random_sample((numSamples,))*(dataRange[-1]-dataRange[0])+dataRange[0]
+    inputX = np.sort(inputX)
+    trueY = transform_x(dataRange)
+    predY=predictFn(model,dataRange)
+    #Plot graphs
+    if showPlot:
+        #A bit of math to determine where to draw the dotted lines
+        plt.figure(figsize = fig_size)
+        #plt.axes().clear()
+        coordX1 = [trainDataRange[0]]*2
+        coordX2 = [trainDataRange[1]]*2
+        minY = min(min(transformed_dataRange),min(predY))
+        maxY = max(max(transformed_dataRange),max(predY))
+        line1, = plt.plot(coordX1, [minY,maxY], 'k-', lw=1,linestyle="--", label="Training Data Range")
+        plt.plot(coordX2, [minY,maxY], 'k-', lw=1,linestyle="--")
+        #TrainX
+        train, = plt.plot(trainX,noisyY,'ko', lw=2, label="Training Data")
+        #Prediction
+        pred, = plt.plot(dataRange,predY,'b-', lw=2, label="Predicted Model")
+        #True Data
+        trueData, = plt.plot(dataRange, transform_x(dataRange),'r-', lw=1.5, label="True Data Distribution",alpha=0.7)
+
+        plt.ylabel("Y")
+        plt.xlabel("Input X")
+        plt.ylim(ymin=min(noisyY-20),ymax=max(noisyY)*2)
+        title = "Lambda = {0}".format(l)
+        for i,key in enumerate(coeffs.keys()):
+            title += " "+str(key)+"="+str(coeffs[key])
+            if i<len(coeffs.keys())-1:
+                title +=","
+        plt.title(title)
+        # Create a legend for the first line.
+        legend = plt.legend(handles=[line1,train,pred,trueData])
+        plt.show()
+    return error(trueY,predY)
+
 #Train the data
 def model(trainX,trainY,degree=1):
     #Creates the vandermonde matrix https://en.wikipedia.org/wiki/Vandermonde_matrix
@@ -90,6 +130,22 @@ def model(trainX,trainY,degree=1):
     model = np.linalg.solve(A.T@A,A.T@trainY)
     return model
 
+def modelWithRidgeRegularization(trainX, trainY, regParam, degree=1):
+    powers=np.vander(trainX,degree+1)
+    A=powers
+    #Solves the normal equation
+    model = np.linalg.solve(A.T@A + regParam*np.eye(degree+1),A.T@trainY)
+    return model
+
+def modelWithLassoRegularization(trainX, trainY, regParam, degree=1):
+    powers=np.vander(trainX,degree+1)
+    A=powers
+    #Solves the normal equation
+    model = Lasso(alpha=regParam)
+    model.fit(A,trainY)
+    return model
+
+    
 #Predicts given x values based on a model
 def predict(model,x):
     degree=len(model)-1
@@ -115,6 +171,33 @@ def overfittingDemo(degrees = [0,1,2,3,4,5,7,10,13]):
         trainError.append(currTrainErr)
         testError.append(currTestErr)
 
+def ridgeRegularizationDemo(lambdas, degree):
+    trainX,trainY = generateTrainData(showPlot=False)
+    trainError = []
+    testError = []
+    #Iterate over all model orders
+    for l in lambdas:
+        currModel = modelWithRidgeRegularization(trainX,trainY,l,degree=degree)
+        predTrainY = predict(currModel,trainX)
+        currTrainErr = error(trainY,predTrainY)
+        currTestErr = validateRegularization(currModel,predict,trainX,trainY,l,showPlot=True)
+        trainError.append(currTrainErr)
+        testError.append(currTestErr)
+
+def lassoRegularizationDemo(lambdas, degree):
+    trainX,trainY = generateTrainData(showPlot=False)
+    trainError = []
+    testError = []
+    #Iterate over all model orders
+    for l in lambdas:
+        currModel = modelWithLassoRegularization(trainX,trainY,l,degree=degree)
+        predTrainY = currModel.predict(np.vander(trainX,degree+1))
+        currTrainErr = error(trainY,predTrainY)
+        currTestErr = validateRegularization(currModel,lambda model, x: model.predict(np.vander(x,degree+1)),trainX,trainY,l,showPlot=True)
+        trainError.append(currTrainErr)
+        testError.append(currTestErr)
+
+
     #Plot the errors
 #     plt.figure(figsize=(10,4))
 #     plt.subplot(1,2,1)
@@ -138,29 +221,29 @@ def get_features(data, col_list, y_name):
     """
     Function to return a numpy matrix of pandas dataframe features, given k column names and a single y column
     Outputs X, a n X k dimensional numpy matrix, and Y, an n X 1 dimensional numpy matrix.
-    This is not a smart function - although it does drop rows with NA values. It might break.
-
+    This is not a smart function - although it does drop rows with NA values. It might break. 
+    
     data(DataFrame): e.g. mpg, mpg_train
     col_list(list): list of columns to extract data from
     y_name(string): name of the column you to treat as the y column
-
+    
     Ideally returns one np.array of shape (len(data), len(col_list)), and one of shape (len(data), len(col_list))
     """
-
+    
     # keep track of numpy values
     feature_matrix = data[col_list + [y_name]].dropna().values
-    return feature_matrix[:, :-1], feature_matrix[:, -1]
-
+    return feature_matrix[:, :-1], feature_matrix[:, -1]    
+    
 def plot_multiple_linear_regression(data, x_1_name, x_2_name, y_name):
     """
     This function makes a 3D plot for multliple linear regression with two explanatory variables.
     Thanks to: https://stackoverflow.com/a/26434204
-
+    
     data(DataFrame): e.g. mpg, mpg_train
     x_1_name(string): the name of the column representing the first explanatory variable
     x_2_name(string): the name of the column representing the second explanatory variable
     y_name(string): the name of the column representing the dependent/response variable
-
+    
     returns None but outputs an interactive 3D plot
     """
     model = LinearRegression()
@@ -176,7 +259,6 @@ def plot_multiple_linear_regression(data, x_1_name, x_2_name, y_name):
 
     exog = pd.core.frame.DataFrame({x_1_name: x_surf.ravel(), x_2_name: y_surf.ravel()})
     out = fit.predict(exog)
-    print(exog.shape)
     ax.plot_surface(x_surf, y_surf,
                     out.reshape(x_surf.shape),
                     rstride=1,
@@ -194,50 +276,4 @@ def plot_multiple_linear_regression(data, x_1_name, x_2_name, y_name):
     plt.title("Linear Model with 2 Explanatory Variables: " + x_1_name + " and " + x_2_name + " vs. " + y_name, y=1.08, fontsize=16)
 
     plt.show();
-
-def plot_simple_residuals(data, x_name, y_name, linear_model):
-    """
-    This function plots a residual plot based off of a simple linear model
-    on top of the scatterplot of the data it was fit to.
-
-    data(DataFrame): e.g. mpg_train
-    x_name(string): the name of the column representing the predictor variable
-    y_name(string): the name of the column representing the dependent/response variable
-    linear_model
-
-    returns None but outputs residual plot resulting from linear model overlaid on scatterplot
-    """
-    X = data[[x_name]].values
-    Y = data[[y_name]].values
-    residuals = Y - linear_model.predict(X)
-
-#     axes = plt.gca()
-    plt.scatter(X, residuals)
-    plt.axhline(y=0, color='r', linestyle='-')
-    plt.title("Residual Plot: " + x_name + " vs. " + y_name)
-    plt.xlabel(x_name)
-    plt.ylabel("Residuals")
-    plt.show()
-
-# This function will just plot a scatter plot with the linear model overlaid.
-def overlay_simple_linear_model(data, x_name, y_name, linear_model):
-    """
-    This function plots a simple linear model on top of the scatterplot of the data it was fit to.
-
-    data(DataFrame): e.g. mpg_train
-    x_name(string): the name of the column representing the predictor variable
-    y_name(string): the name of the column representing the dependent/response variable
-    linear_model
-
-    returns None but outputs linear model overlaid on scatterplot
-    """
-
-    x = np.arange(max(data[x_name])).reshape(-1, 1) # a 2D array of integers between 0 and the maximum value of the x_name column
-    y = linear_model.predict(x) # replace ___ with correct function
-
-
-    data.plot.scatter(x_name, y_name) # scatter plot of x_name vs. y_name
-
-    plt.plot(x, y, color='red')
-    plt.title("Linear Model vs. Data: " + x_name + " vs. " + y_name)
-    plt.show()
+    
